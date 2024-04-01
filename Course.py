3e1f1694a8d89faeceabee0744b72774829ac02b
AymanQashoo <165571338+AymanQashoo@users.noreply.github.com>
2024-04-01 22:54:32 +0300

    Add files via upload

---
+++ Course.py
@@ -0,0 +1,11 @@
+import queue
+
+class Course1():
+
+    def __init__(self, name, count_prerequisites, cridit):
+        self.name = name
+        self.count_prerequisites = count_prerequisites
+        self.cridit = cridit
+    queue = queue.Queue()
+
+#def PRIO(self):
